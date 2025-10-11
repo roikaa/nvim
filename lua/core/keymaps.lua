4b22aed00037648b66b5a7c636004e384c9c9b35
roikaa <akio216216@gmail.com>
2025-10-11 17:05:20 +0100

    Finely?

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,6 +5,8 @@ vim.g.maplocalleader = " "
 -- Disable the spacebar key's default behavior in Normal and Visual modes
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
+local opts = { noremap = true, silent = true }
+
 -- save file
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
 
@@ -13,3 +15,59 @@ vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
 
 -- quit file
 vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+
+-- Find and center
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
+
+-- Resize with arrows
+vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+
+-- Buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>bx", ":bdelete!<CR>", opts) -- close buffer
+vim.keymap.set("n", "<leader>bn", "<cmd> enew <CR>", opts) -- new buffer
+
+-- Window management
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
+vim.keymap.set("n", "<leader>we", "<C-w>=", opts) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>wx", ":close<CR>", opts) -- close current split window
+
+-- -- Terminal splits
+-- vim.keymap.set("n", "<leader>h", ":split term<cr>", { silent = true })
+-- vim.keymap.set("n", "<leader>v", ":vsplit term<cr>", { silent = true })
+
+-- Navigate between splits
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+
+-- Tabs
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
+
+-- Toggel line wrap
+vim.keymap.set("n", "<leader>W", "<cmd>set wrap!<CR>", opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set("v", "p", '"_dP', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to previous diagnostic message" })
+
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to next diagnostic message" })
+
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
