3fe8d414770264f752e4234ef18b91d469ac0b97
roikaa <akio216216@gmail.com>
2025-10-11 10:07:15 +0100

    anoying lsp

--- init.lua
+++ init.lua
@@ -1,37 +1,36 @@
-require 'core.options' -- Load general options
-require 'core.keymaps' -- Load general keymaps
-
+require("core.options") -- Load general options
+require("core.keymaps") -- Load general keymaps
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  require 'plugins.gruvbox',
-  require 'plugins.telescope',
-  require 'plugins.alpha',
-  require 'plugins.treesitter',
-  require 'plugins.bufferline',
-  require 'plugins.autocompletion',
-  require 'plugins.comment',
-  require 'plugins.neotree',
-  -- require 'plugins.lualine',
-  -- require 'plugins.lsp',
-  -- require 'plugins.none-ls',
-  -- require 'plugins.gitsigns',
-  -- require 'plugins.indent-blankline',
-  -- require 'plugins.misc',
+	require("plugins.gruvbox"),
+	require("plugins.telescope"),
+	require("plugins.alpha"),
+	require("plugins.treesitter"),
+	require("plugins.bufferline"),
+	require("plugins.autocompletion"),
+	require("plugins.comment"),
+	require("plugins.neotree"),
+	require("plugins.lualine"),
+	-- require("plugins.lsp"),
+	require("plugins.none-ls"),
+	-- require 'plugins.gitsigns',
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
 })
