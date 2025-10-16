6a13dc406f4501055f84557589ffaeea268a0d86
roikaa <akio216216@gmail.com>
2025-10-16 10:23:37 +0100

    ifogor

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,68 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
-    opts = {
-      ensure_installed = {
-        'nix',
-        'lua',
-        'python',
-        'javascript',
-        'typescript',
-        'vimdoc',
-        'vim',
-        'regex',
-        'terraform',
-        'sql',
-        'dockerfile',
-        'toml',
-        'json',
-        'java',
-        'groovy',
-        'go',
-        'gitignore',
-        'graphql',
-        'yaml',
-        'make',
-        'cmake',
-        'markdown',
-        'markdown_inline',
-        'bash',
-        'tsx',
-        'css',
-        'html',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require('treesitter-context').setup({
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        multiwindow = false, -- Enable multiwindow support.
-        max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs",
+		opts = {
+			ensure_installed = {
+				"nix",
+				"latex",
+				"lua",
+				"python",
+				"javascript",
+				"typescript",
+				"vimdoc",
+				"vim",
+				"regex",
+				"terraform",
+				"sql",
+				"dockerfile",
+				"toml",
+				"json",
+				"java",
+				"groovy",
+				"go",
+				"gitignore",
+				"graphql",
+				"yaml",
+				"make",
+				"cmake",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"tsx",
+				"css",
+				"html",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = { "latex" }, -- Disable tree-sitter for latex
+				additional_vim_regex_highlighting = false,
+				-- additional_vim_regex_highlighting = { "latex" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				multiwindow = false, -- Enable multiwindow support.
+				max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+				line_numbers = true,
+				multiline_threshold = 20, -- Maximum number of lines to show for a single context
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = nil,
+				zindex = 20, -- The Z-index of the context window
+				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+			})
+		end,
+	},
 }
