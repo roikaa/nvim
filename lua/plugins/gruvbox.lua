957838d96582d672e642fcd2b9e6f4812f546e04
roikaa <akio216216@gmail.com>
2026-02-09 15:05:08 +0100

    styling

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,12 +1,11 @@
-return { 
-  "ellisonleao/gruvbox.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("gruvbox").setup({
-      -- Optional: customize gruvbox settings
-      -- contrast = "hard", -- hard, soft, or empty string
-    })
-    vim.cmd([[colorscheme gruvbox]])
-  end,
+return {
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- Optionally configure and load the colorscheme
+		-- directly inside the plugin declaration.
+		vim.g.gruvbox_material_enable_italic = true
+		vim.cmd.colorscheme("gruvbox-material")
+	end,
 }
