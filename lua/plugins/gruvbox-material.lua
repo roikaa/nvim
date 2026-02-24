53f7e224d2a9e34f42b9b51fb53114501ce95e5d
roikaa <akio216216@gmail.com>
2026-02-24 22:11:34 +0100

    styling

--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -1,12 +1,35 @@
 return {
-	"sainnhe/gruvbox-material",
+	-- "sainnhe/gruvbox-material",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	-- Optionally configure and load the colorscheme
+	-- 	-- directly inside the plugin declaration.
+	-- 	vim.g.gruvbox_material_transparent_background = 2
+	-- 	vim.g.gruvbox_material_enable_italic = true
+	-- 	vim.cmd.colorscheme("gruvbox-material")
+	-- end,
+	"f4z3r/gruvbox-material.nvim",
+	name = "gruvbox-material",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
-		vim.g.gruvbox_material_transparent_background = 2
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
+	opts = {
+		italics = true,
+		contrast = "hard", -- or "medium", "soft"
+		comments = {
+			italics = true,
+		},
+		background = {
+			transparent = false,
+		},
+		float = {
+			force_background = false,
+			background_color = nil,
+		},
+		signs = {
+			force_background = false,
+			background_color = nil,
+		},
+		customize = nil,
+	},
 }
