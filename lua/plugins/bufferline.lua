7dc5c2c03029bb559942dd93b9c098dd78981712
roikaa <akio216216@gmail.com>
2026-02-24 17:04:05 +0100

    style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -28,8 +28,8 @@ return {
         show_buffer_close_icons = true,
         show_close_icon = true,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-       -- separator_style = { '│', '│' }, -- | "thick" | "thin" | { 'any', 'any' },
-        separator_style = "slant", --| "slope" | "thick" | "thin" | { 'any', 'any' },
+       separator_style = { '│', '│' }, -- | "thick" | "thin" | { 'any', 'any' },
+        -- separator_style = "slant", --| "slope" | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = true,
         always_show_bufferline = true,
         show_tab_indicators = false,
